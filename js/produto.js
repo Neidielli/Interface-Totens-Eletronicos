f0a3672b4809929965cda065b3c8cfef137dbf01
Leandro Makino Marangoni <leandro_marangoni@outlook.com>
2022-06-20 12:01:30 -0300

    Correção do Carrinho 2

--- js/produto.js
+++ js/produto.js
@@ -16,11 +16,12 @@ function addCarrinhoClicado(event){
     var id = shopProdutos.id;
     var nomeValor = shopProdutos.getElementsByClassName("produto-titulo")[0].innerText; 
     var valor = shopProdutos.getElementsByClassName("produto-preco")[0].innerText.replace("R$",""); 
-    var quantidade = shopProdutos.getElementsByClassName("produto-quantidade").;
-    console.log(quantidade)
-    /*var itemNumero = parseInt(localStorage.getItem('itemNumero')) + 1;
-    localStorage.setItem("itemNumero", itemNumero);*/
+    var quantidade = shopProdutos.getElementsByClassName("produto-quantidade")[0].value;
+    console.log(quantidade);
+    var departamento = shopProdutos.getElementsByClassName("produto-departamento")[0].innerText.replace("Home / ","")
+   
     localStorage.setItem(id + " nome", nomeValor);
     localStorage.setItem(id + " valor", valor);
-    localStorage.setItem(id + " quantidadeProduto", quantidade);
+    localStorage.setItem(id + " quantidade", quantidade);
+    localStorage.setItem(id + " departamento", departamento);
 }
