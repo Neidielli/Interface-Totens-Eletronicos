a14e17b2c703bbc3801488b8da715312604e641d
Leandro Makino Marangoni <leandro_marangoni@outlook.com>
2022-06-20 15:45:41 -0300

    Barra de Pesquisa Pronta

--- js/estilo.js
+++ js/estilo.js
@@ -1,4 +1,5 @@
 let barraPesquisa = document.querySelector(".form-control");
+
 barraPesquisa.onkeyup = () => {
     var filtro,menu,menuItens,links;
     
@@ -16,6 +17,52 @@ barraPesquisa.onkeyup = () => {
     }
 }
 
+barraPesquisa.focusin = () => {
+    var filtro,menu,menuItens,links;
+    
+    filtro = barraPesquisa.value.toUpperCase();
+    menu = document.getElementById("menu");
+    menuItens = menu.getElementsByTagName("li");
+    for(var i = 0; i < menuItens.length; i++){
+        links = menuItens[i].getElementsByTagName("a")[0];
+        console.log(links.innerHTML.toUpperCase().indexOf(filtro))
+        if(links.innerHTML.toUpperCase().indexOf(filtro)> -1){
+            menuItens[i].style.display="block";
+        }else if(filtro == "" || links.innerHTML.toUpperCase().indexOf(filtro) == -1){
+            menuItens[i].style.display="none";
+        }
+    }
+}
+
+barraPesquisa.focusout = () => {
+    var filtro,menu,menuItens,links;
+    
+    filtro = barraPesquisa.value.toUpperCase();
+    menu = document.getElementById("menu");
+    menuItens = menu.getElementsByTagName("li");
+    for(var i = 0; i < menuItens.length; i++){
+        links = menuItens[i].getElementsByTagName("a")[0];
+        console.log(links.innerHTML.toUpperCase().indexOf(filtro))
+        if(links.innerHTML.toUpperCase().indexOf(filtro)> -1){
+            menuItens[i].style.display="block";
+        }else if(filtro == "" || links.innerHTML.toUpperCase().indexOf(filtro) == -1){
+            menuItens[i].style.display="none";
+        }
+    }
+}
+
+barraPesquisa.focusout = () => {
+    var filtro,menu,menuItens,links;
+    
+    filtro = barraPesquisa.value.toUpperCase();
+    menu = document.getElementById("menu");
+    menuItens = menu.getElementsByTagName("li");
+    for(var i = 0; i < menuItens.length; i++){
+        links = menuItens[i].getElementsByTagName("a")[0];
+        menuItens[i].style.display="none";
+        
+    }
+}
 // FAZENDO A FUNÇÃO DE PESQUISAR
 function pesquisar(){
 
