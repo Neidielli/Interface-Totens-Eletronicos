1ae6338335bd9a46c0fad220b0d6cf80c71e1479
Neidielli <100215137+Neidielli@users.noreply.github.com>
2022-06-20 14:31:07 -0300

    js da pesquisa

--- js/estilo.js
+++ js/estilo.js
@@ -1,3 +1,4 @@
+// FAZENDO A FUNÇÃO DE PESQUISAR
 function pesquisar(){
     var input,filtro,menu,menuItens,links;
     input = document.getElementById("pesquisa");
@@ -12,4 +13,13 @@ function pesquisar(){
             menuItens[i].style.display="none";
         }
     }
+}
+
+// DEIXANDO A BARRA DE PESQUISA DINÂMICA
+let pesquisaIcon = document.querySelector(".pesquisaIcone");
+let pesquisaConteudo = document.querySelector("#menu");
+let fecharPesquisa = document.querySelector(""); /* FECHAR QUANDO O USUÁRIO CLICAR EM OUTRA COISA*/
+
+pesquisaIcon.onclick = () => {
+    pesquisaConteudo.classList.add(""); /* TORNAR ATIVO QUANDO REALIZAR A BUSCA */
 }
