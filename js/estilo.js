44c9c7a2570853a334c5aa0b62a6858f7a14b99e
Leandro Makino Marangoni <leandro_marangoni@outlook.com>
2022-06-20 15:27:16 -0300

    barra de pesquisa

--- js/estilo.js
+++ js/estilo.js
@@ -1,25 +1,28 @@
-// FAZENDO A FUNÇÃO DE PESQUISAR
-function pesquisar(){
-    var input,filtro,menu,menuItens,links;
-    input = document.getElementById("pesquisa");
-    filtro = input.value.toUpperCase();
+let barraPesquisa = document.querySelector(".form-control");
+barraPesquisa.onkeyup = () => {
+    var filtro,menu,menuItens,links;
+    
+    filtro = barraPesquisa.value.toUpperCase();
     menu = document.getElementById("menu");
     menuItens = menu.getElementsByTagName("li");
     for(var i = 0; i < menuItens.length; i++){
         links = menuItens[i].getElementsByTagName("a")[0];
-        if(links.innerHTML.toUpperCase().indexOf(filtro)>-1){
-            menuItens[i].style.display="";
-        }else{
+        console.log(links.innerHTML.toUpperCase().indexOf(filtro))
+        if(links.innerHTML.toUpperCase().indexOf(filtro)> -1){
+            menuItens[i].style.display="block";
+        }else if(filtro == "" || links.innerHTML.toUpperCase().indexOf(filtro) == -1){
             menuItens[i].style.display="none";
         }
     }
 }
 
+// FAZENDO A FUNÇÃO DE PESQUISAR
+function pesquisar(){
+
+}
+
 // DEIXANDO A BARRA DE PESQUISA DINÂMICA
-let pesquisaIcon = document.querySelector(".pesquisaIcone");
+/*let pesquisaIcon = document.querySelector(".pesquisaIcone");
 let pesquisaConteudo = document.querySelector("#menu");
 let fecharPesquisa = document.querySelector(""); /* FECHAR QUANDO O USUÁRIO CLICAR EM OUTRA COISA*/
 
-pesquisaIcon.onclick = () => {
-    pesquisaConteudo.classList.add(""); /* TORNAR ATIVO QUANDO REALIZAR A BUSCA */
-}
